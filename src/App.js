9f663d0ff9f294dc4f14d3918fda641861012c91
PatipatPhuttasuk <5835512104@psu.ac.th>
2018-09-13 10:56:22 +0700

    check 2.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
+const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello World
- </div>
+      <div><center>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+        }
+      </center></div>
     );
   }
 }
