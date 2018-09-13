7c7950173e51e6e8b9ddbdd7d438b3677d7827ae
PatipatPhuttasuk <5835512104@psu.ac.th>
2018-09-13 11:26:50 +0700

    check 2-3

---
+++ src/WordCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import CharacterCard from './CharacterCard';
+
+export default class WordCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            attempt: '',
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                {Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} />)}
+            </div>
+        );
+    }
+}
