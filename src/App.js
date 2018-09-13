1c17978dd97a107972d1fc25e4fef8e7ebf69f6d
PatipatPhuttasuk <5835512104@psu.ac.th>
2018-09-13 10:46:05 +0700

    check 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CharacterCard value="h" />
-        <CharacterCard value="i" />
-      </div>
+        Hello World
+ </div>
     );
   }
 }
-
 export default App;
